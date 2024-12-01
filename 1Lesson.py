ec777e364cbf4c0e104b5ad934c7263ee32650db
student <roma.petliuk.11@gmail.com>
2024-12-01 14:53:19 +0200

    Add 0

--- 1Lesson.py
+++ 1Lesson.py
@@ -1,14 +1,5 @@
-a = int(input("Введіть 1 значення "))
-b = int(input("Введіть 2 значення "))
-c = int(input("Введіть 3 значення "))
-userage = int(input("колько те лет? "))
-
-if userage >= 40:(
-    print("ти СКУФ")
-)
-else:(
-    print("ти не скуф")
-)
+a = int(input("Введіть значення a "))
+b = int(input("Введіть значення b "))
 
 print("a + b = ")
 print(a + b)
@@ -16,26 +7,8 @@ print("a - b = ")
 print(a - b)
 print("a * b = ")
 print(a * b)
-print("a : b = ")
-print(a / b)
-print("a + b + c = ")
-print(a + b + c)
-print("a - b - c= ")
-print(a - b - c)
-print("a * b * c = ")
-print(a * b * c)
-print("a : b : c= ")
-print(a / b / c)
-print("great numbers")
-
-print("Cant u calculate by yourself?")
-
-if a <= 100:(
-    print("small number a")
-)
-if b <= 100:(
-    print("small number b")
-)
-if c <= 100:(
-    print("small number c")
-)
+if b == 0:
+    print("На 0 ділити не можна")
+else:
+    print("a / b = ")
+    print(a / b)
