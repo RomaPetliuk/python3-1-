3567d0671b1566be9fd0acb6f161f99d4f829430
Petlyuk-Vladimir <vladimir.petliuk@y-tree.com>
2024-12-02 22:37:29 +0200

    Add major calculation operations

--- 1Lesson.py
+++ 1Lesson.py
@@ -1,14 +1,20 @@
-a = int(input("Введіть значення a "))
-b = int(input("Введіть значення b "))
+a = float(input("Введіть значення a = "))
+b = float(input("Введіть значення b = "))
+UserS = input("Введите знак (+, -, *, /) ")
 
-print("a + b = ")
-print(a + b)
-print("a - b = ")
-print(a - b)
-print("a * b = ")
-print(a * b)
-if b == 0:
-    print("На 0 ділити не можна")
-else:
-    print("a / b = ")
-    print(a / b)
+if UserS == "+":
+    result = a + b
+    print("a + b =",result)
+if UserS == "-":
+    result = a - b
+    print("a - b =", result)
+if UserS == "*":
+    result = a * b
+    print("a * b =", result)
+if UserS == "/":
+    if b == 0:
+        print("На 0 ділити не можна")
+    else:
+        result = a / b
+        print("a / b =", result)
+        print(round(result))
