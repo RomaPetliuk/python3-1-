99c7f7585f92db082f411f09aa081cf79680b6bf
Petlyuk-Vladimir <vladimir.petliuk@y-tree.com>
2024-12-04 22:40:53 +0200

    Add Directory for class work 1

--- HW1/HM2.py
+++ HW1/HM2.py
@@ -1,6 +1,6 @@
 Usergrade = int(input("Ведите свою оценку: "))
 
-if Usergrade > 0 and Usergrade <= 49:
+if Usergrade >= 0 and Usergrade <= 49:
     print("Незадовольнительно")
 elif Usergrade >= 50 and Usergrade <= 69:
     print("Нормально")
