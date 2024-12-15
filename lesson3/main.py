8c12658558112f5f7f89f1b9f1c95b483952da9a
student <roma.petliuk.11@gmail.com>
2024-12-15 14:49:34 +0200

    Add new class Tank and corected am few moments in the main.py

--- lesson3/main.py
+++ lesson3/main.py
@@ -1,27 +1,31 @@
 from lesson5.berserk import Berserk
 from character import Character
+from lesson5.berserk import Tank
 
 
-player1 = Character("Andrey", 110, 7, 0)
+player1 = Tank("Andrey", 110, 10, 10)
 player1.print_stats()
 
 player2 = Berserk("Gde Krishichka", 100, 15, 0)
 player2.print_stats()
 
-player3 = Character("Ekaterina Mezylina", 100, -10, -5)
+player3 = Character("Ekaterina Mezylina", 100, 15, -5)
 player3.print_stats()
 
 
 while player1.health > 0 and player2.health > 0 and player3.health > 0:
     player1_damage = player1.attack(player2)
     print(f"{player1.name} атакував {player2.name} і наніс {player1_damage} шкоди")
-    print(f"у {player1.name} лишилося {player1.health} здоровя")
+    print(f"у {player2.name} лишилося {player2.health} здоров\'я")
+    print('')
     player2_damage = player2.attack(player3)
     print(f"{player2.name} атакував {player3.name} і наніс {player2_damage} шкоди")
-    print(f"у {player3.name} лишилося {player3.health} здоровя")
-    player3_damage = player3.attack(player2)
-    print(f"{player3.name} атакував {player2.name} і наніс {player3_damage} шкоди")
-    print(f"у {player2.name} лишилося {player2.health} здоровя")
+    print(f"у {player3.name} лишилося {player3.health} здоров\'я")
+    print('')
+    player3_damage = player3.attack(player1)
+    print(f"{player3.name} атакував {player1.name} і наніс {player3_damage} шкоди")
+    print(f"у {player1.name} лишилося {player1.health} здоров\'я")
+    print('')
 print("\n\n")
 player1.print_stats()
 player2.print_stats()
--- lesson5/berserk.py
+++ lesson5/berserk.py
@@ -1,4 +1,21 @@
 from lesson3.character import Character
 class Berserk(Character):
-    def __init__(self, name, health=1000, damage=200, defense=0):
+    damage_multiplier = 15
+    rage_limit = 60;
+    def __init__(self, name, health=1000, damage=200, defense=0, damage_multiplier = 15, rage_limit = 60 ):
         Character.__init__(self, name, health, damage, defense)
+        self.damage_multiplier = damage_multiplier
+        self.rage_limit = rage_limit
+
+    def attack(self, target):
+        final_damage = self.damage * self.damage_multiplier if self.health < self.rage_limit else self.damage
+        return target.take_damage(final_damage)
+
+class Tank(Character):
+    def __init__(self, name,health=100, damage=20, defence=10):
+        Character.__init__(self, name, health, damage, defence)
+
+    def take_damage(self, damage):
+        final_damage = damage - self.defence if damage >= self.defence else 0
+        self.health = max(self.health - final_damage, 0)
+        return final_damage
