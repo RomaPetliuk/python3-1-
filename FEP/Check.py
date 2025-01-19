ca00b66529b60352a58d57f2e3dbf044f9204070
student <roma.petliuk.11@gmail.com>
2025-01-19 15:10:07 +0200

    add new final exams

--- FEP/Check.py
+++ FEP/Check.py
@@ -14,10 +14,8 @@ peoples = [
     Person(name="Marina", money = 100, mood = 150, health = 100),
     Person(name="Vova", money = 50000, mood = 100, health = 90)
 ]
-actions = [
-    Action("Робота", money=random.randint(10, 50), health=random.randint(-10, -5), mood=random.randint(-10, -3)),
-    Action("Відпочинок", money=random.randint(-15, -5), health=random.randint(1, 5), mood=random.randint(1, 5)),
-]
+
+go_to_work = Action("Робота", money=random.randint(10, 50), health=random.randint(-10, -5), mood=random.randint(-10, -3)),
 while True:
     for person in peoples:
         person.change_state(
