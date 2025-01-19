9a16fe44211fb4e305d3da782b0ca074107336db
student <roma.petliuk.11@gmail.com>
2025-01-19 15:09:19 +0200

    add new final exams

--- FEP/Check.py
+++ FEP/Check.py
@@ -1,7 +1,7 @@
 from FEP1 import Person
-human = Person(name='Тарас', money=0, mood=100, health=100)
-
-print(human)
+from FEP1 import Action
+import random
+human = Person(name='Roma', money=0, mood=100, health=100)
 
 human.change_state(
          money = 100,
@@ -9,5 +9,29 @@ human.change_state(
          health = -5
      )
 
-print(human)
-print((()))
+peoples = [
+    Person(name="Roma", money = 500, mood = 100, health = 80),
+    Person(name="Marina", money = 100, mood = 150, health = 100),
+    Person(name="Vova", money = 50000, mood = 100, health = 90)
+]
+actions = [
+    Action("Робота", money=random.randint(10, 50), health=random.randint(-10, -5), mood=random.randint(-10, -3)),
+    Action("Відпочинок", money=random.randint(-15, -5), health=random.randint(1, 5), mood=random.randint(1, 5)),
+]
+while True:
+    for person in peoples:
+        person.change_state(
+            money = random.randint(-10, -5),
+            mood = random.randint(-10, -5),
+            health = random.randint(-10, -5)
+        )
+        if person.health <= 0:
+            peoples.remove(person)
+        if person.mood <= 0:
+            peoples.remove(person)
+        if person.money <= 0:
+            peoples.remove(person)
+
+        if len(peoples) == 0:
+            print("persons are empty")
+            break
